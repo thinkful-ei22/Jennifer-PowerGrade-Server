0c6f57ea154d0bb2b3d5047f86eb1d6c378fb28d
Jennifer Colna <jennifer.colna@gmail.com>
2018-08-06 10:13:12 -0500

    fixed config

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/powerGradeDb',
-  TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost/test-powerGrade',
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/server',
+  TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost/test-server',
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d',
   DATABASE_URL:

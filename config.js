8e2f01115c338adaab4b93b3202ea42b15174fba
Jennifer Colna <jennifer.colna@gmail.com>
2018-08-06 10:15:50 -0500

    updated config

--- config.js
+++ config.js
@@ -4,12 +4,12 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/server',
-  TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost/test-server',
+  TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost/server-test',
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d',
   DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
+        process.env.DATABASE_URL || 'mongodb://localhost/server',
   TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
-        'mongodb://localhost/thinkful-backend-test'
+        'mongodb://localhost/server-test'
 };

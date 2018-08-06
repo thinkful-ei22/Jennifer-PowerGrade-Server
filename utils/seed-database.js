68e7040594d9e5451f65c269745412f2263e9d19
Jennifer Colna <jennifer.colna@gmail.com>
2018-08-06 16:57:32 -0500

    fixed seed database

--- utils/seed-database.js
+++ utils/seed-database.js
@@ -9,6 +9,10 @@ const Student = require('../models/student');
 const seedStudents = require('../database/seed/students');
 const User = require('../models/user');
 const seedUsers = require('../database/seed/users');
+const Grade = require('../models/grade');
+const seedGrades = require('../database/seed/grades');
+const Category = require('../models/category');
+const seedCategories = require('../database/seed/categories');
 mongoose.connect(DATABASE_URL)
   .then(() => mongoose.connection.db.dropDatabase())
   .then(() => {
@@ -20,13 +24,19 @@ mongoose.connect(DATABASE_URL)
       Student.insertMany(seedStudents),
       Student.createIndexes(),
       User.insertMany(seedUsers),
-      User.createIndexes()
+      User.createIndexes(),
+      Grade.insertMany(seedGrades),
+      Grade.createIndexes(),
+      Category.insertMany(seedCategories),
+      Category.createIndexes()
     ])
       .then(results => {
         console.info(`Inserted ${results[0].length} Assignments`);
         console.info(`Inserted ${results[2].length} Classes`); 
         console.info(`Inserted ${results[4].length} Students`);
-        console.info(`Inserted ${results[6].length} Users`);   
+        console.info(`Inserted ${results[6].length} Users`);  
+        console.info(`Inserted ${results[8].length} Grades`); 
+        console.info(`Inserted ${results[10].length} Categories`);  
       })
       .then(() => mongoose.disconnect())
       .catch(err => {

d0fe9261f15e32bb08bb6064398e007862c1c954
Jennifer Colna <jennifer.colna@gmail.com>
2018-08-17 10:25:00 -0500

    secret

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_SECRET: process.env.JWT_SECRET || 'secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d',
   DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'mongodb://localhost/thinkful-backend-test'

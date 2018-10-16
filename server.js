5aeda975243eb516060eb0ce8bb4307fd4ed7f8c
Jennifer Colna <jennifer.colna@gmail.com>
2018-10-16 18:21:45 -0500

    removed test

--- server.js
+++ server.js
@@ -30,11 +30,6 @@ app.use(
   })
 );
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
 app.use(express.json());
 passport.use(localStrategy);
 passport.use(jwtStrategy);

9781d7c34331a49da2a73c5591a3489a8ce3c723
Jennifer Colna <jennifer.colna@gmail.com>
2018-10-16 18:14:40 -0500

    trying to fix access origin

--- server.js
+++ server.js
@@ -30,6 +30,11 @@ app.use(
   })
 );
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.use(express.json());
 passport.use(localStrategy);
 passport.use(jwtStrategy);
